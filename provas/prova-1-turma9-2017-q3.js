2a49a9a17f8997d1362af3bba5ff0207ecf01615
JosineideSS <josineide1402@gmail.com>
2019-07-29 23:55:44 -0300

    Update prova-1-turma9-2017-q3.js

--- provas/prova-1-turma9-2017-q3.js
+++ provas/prova-1-turma9-2017-q3.js
@@ -1,7 +1,7 @@
 var x = prompt('x: ')
 var y = prompt('y: ')
 //O operador && significa and, ele só é verdadeiro se todas as condições forem
-//O operador != significa direrente
+//O operador != significa diferente
 if(x > 0 && y > 0) {
     alert('Q1')
 } else if(x < 0 && y > 0){
