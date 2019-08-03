9768f3dc05176fe7491f57b488acd2f95830a85e
JosineideSS <josineide1402@gmail.com>
2019-08-03 07:22:55 -0300

    O mês por extenso

---
+++ questoes/O mês por extenso.js
@@ -0,0 +1,17 @@
+var dia= prompt("dia: ")
+var mes = prompt("mes")
+var ano = prompt("ano: ")
+var meses = {1:"Janeiro",
+             2: "Fevereiro",
+             3:"Março",
+             4:"Abril",
+             5:"Maio",
+             6: "Junho",
+             7: "Julho",
+             8: "Agosto",
+             9: "Setembro",
+             10: "Outubro",
+             11: "Novembro",
+             12: "Dezembro"
+}
+alert(dia+" de "+meses[mes]+" de "+ano)
