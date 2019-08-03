0f48a29bdab96d0b0cabfdce0c917e5c662f926f
JosineideSS <josineide1402@gmail.com>
2019-08-03 06:08:13 -0300

    Polegadas, pés, jardas e milhas

---
+++ questoes/Polegadas, pés, jardas e milhas.js
@@ -0,0 +1,6 @@
+var metros = prompt("metros: ")
+var po = metros / 0.0254
+var pe = metros / 0.3048
+var ja = metros / 0.9144
+var mi = metros / 1604.38
+alert("polegadas: "+po+"\npes: "+pe+"\njardas: "+ja+"\nmilhas: "+mi)
