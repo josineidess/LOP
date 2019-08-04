8c16648faaf208674bc791868215542f17bf51d0
JosineideSS <josineide1402@gmail.com>
2019-08-04 00:06:43 -0300

    L2 Q6

---
+++ questoes/Intervalo fechado.js
@@ -0,0 +1,11 @@
+var array = []
+var a = array[0] = prompt('a: ')
+var b = array[1] = prompt('b: ')
+var c = array[2] = prompt('c: ')
+//ordena em ordem crescente os elementos da lista
+array = array.sort()
+if(c == array[1]){
+    alert("Pertence")
+}else{
+    alert("Nao pertence")
+}
