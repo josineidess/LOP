8cd9dfa268c50b07b33c27157a9646e747e19c9e
JosineideSS <josineide1402@gmail.com>
2019-08-04 01:18:04 -0300

    Update O maior valor de cada vetor.js

--- questoes/O maior valor de cada vetor.js
+++ questoes/O maior valor de cada vetor.js
@@ -1,14 +1,20 @@
 var vetora = []
 var vetorb = []
 var vetorc = []
+//laço for adiciona 10 elementos digitados pelo usuário a lista
 for(let e = 0; e < 10;e ++){
     let el = prompt('num: ')
     vetora.push(el)
+    //converte em inteiro
+    vetora[e] = parseInt(vetora[e])
 }
+//laço for adiciona 10 elementos digitados pelo usuário a lista
 for(let e = 0;e < 10;e++){
     let el = prompt('num: ')
     vetorb.push(el)
+    vetorb[e] = parseInt(vetorb[e])
 }
+//laço for compara as listas
 for(let e = 0; e < 10; e ++) {
     if(vetora[e] > vetorb[e]){
         vetorc.push(vetora[e])
