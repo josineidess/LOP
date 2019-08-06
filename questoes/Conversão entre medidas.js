e4183de8700c9f41bbc37cd45a7ca732783f09ba
JosineideSS <josineide1402@gmail.com>
2019-08-06 06:34:00 -0300

    Conversão entre medidas

---
+++ questoes/Conversão entre medidas.js
@@ -0,0 +1,6 @@
+var metros = prompt("metro: ")
+var pol = metros * 39.3701
+var pes = metros * 3.28084
+var jar = metros * 1.09361
+var mi = metros * 0.000621371
+alert(metros + " metros é "+pol+" polegadas\n"+metros+" metros é "+pes+" pés\n"+metros+" metros é "+jar+" jardas\n"+metros+" metros é "+mi+" milhas")
