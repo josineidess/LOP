9bf761cc3b92b9fc461a6fab3a9498d2091d24bd
JosineideSS <josineide1402@gmail.com>
2019-09-28 00:59:59 -0300

    Update Eh primo ou nao.js

--- questoes/Eh primo ou nao.js
+++ questoes/Eh primo ou nao.js
@@ -1,3 +1,27 @@
+//outra forma de fazer, sem usar for...
+var a = parseInt(prompt("a:"))
+var b = parseInt(prompt("n:"))
+var maior = Math.max(a,b)
+var menor = Math.min(a,b)
+resto = maior - menor
+if(menor > resto){
+var result = 1
+while(resto!=0){
+   result = resto
+   maior = menor
+   menor = resto
+   resto = maior - menor
+}
+}else{
+    if(maior%menor!=0){
+        result=1
+    }else{
+    result=menor}
+}
+alert("O MDC é: "+result)
+
+
+
 var num = parseFloat(prompt("num: "))
 var m = 'Eh primo'
 
