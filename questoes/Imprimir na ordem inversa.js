4691b3564679602bcf67d3a2d0b6de2ed51a0fdc
JosineideSS <josineide1402@gmail.com>
2019-11-01 15:03:12 -0300

    Update Imprimir na ordem inversa.js

--- questoes/Imprimir na ordem inversa.js
+++ questoes/Imprimir na ordem inversa.js
@@ -7,3 +7,19 @@ for(var e = 0;e < 10;e++){
 var b = a.reverse()
 var f = b.join(" ")
 alert(f)
+
+*/outra forma de fazer/*
+var vetor_a = []
+var vetor_b = []
+var i = 9
+var msg = ''
+for(var e = 0; e < 10;e++){
+    var n = parseInt(prompt("num: "))
+    vetor_a[e] = n
+    vetor_b[i] = n
+    i--
+}
+for(var r = 0; r < 10;r++){
+    msg+=vetor_b[r]+" "
+}
+alert(msg)
