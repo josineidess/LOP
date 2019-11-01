93f96a6b5c90a1625d7fdf35d4707bae685eaccd
JosineideSS <josineide1402@gmail.com>
2019-11-01 15:28:17 -0300

    Update Avaliar preço de produtos.js

--- questoes/Avaliar preço de produtos.js
+++ questoes/Avaliar preço de produtos.js
@@ -1,3 +1,35 @@
+var vetor = []
+var n = parseInt(prompt("num: "))
+var menor_que_50 = 0
+var de_50_a_100 = 0
+var maior_100 = 0
+var media_100 = 0
+for(var e = 0; e < n;e++){
+    var preco = parseFloat(prompt("preco"))
+    vetor[e] = preco
+    if(preco < 50){
+        menor_que_50++
+    }else{
+      if(preco > 100){
+      media_100+=preco
+      maior_100++
+      }else{
+        de_50_a_100++
+      
+      }
+    }
+}
+var media_fin = 0
+if(maior_100 != 0){
+    media_fin = media_100/maior_100
+}
+alert("Quantidade de produtos menores que 50 R$: "+menor_que_50+
+"\nQuantidade de produtos entre 50 R$ e 100 R$: "+de_50_a_100+
+"\nQuantidade de produtos media dos maiores que 100 R$: "+(media_fin))
+
+/*usando else if*/
+
+
 var quant = prompt("quantidade de produtos: ")
 var vetor_p = []
 var r = quant
