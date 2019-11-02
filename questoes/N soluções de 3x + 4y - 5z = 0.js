4b02f1e948252d1c72b346c3fdcca5aa06f7b7e5
JosineideSS <josineide1402@gmail.com>
2019-11-02 16:55:52 -0300

    Create N soluções de 3x + 4y - 5z = 0.js

---
+++ questoes/N soluções de 3x + 4y - 5z = 0.js
@@ -0,0 +1,30 @@
+var vetorz = [],vetory = [],vetorx = []
+var n = parseInt(prompt("num: "))
+var p = n
+var vezes = (n+1)*(n+1)*(n+1)
+var msg = ''
+for(var e = 0,x=-1,y=0,z=0;e < vezes;e++){
+   if(z > n){
+     z = 0
+   }
+   vetorz[e] = z
+   z++
+   if(e%(n+1) === 0){
+      y++
+    }
+    if(e % ((n+1)**2) === 0){
+      y = 0
+      x++
+    }
+    vetorx[e] = x
+    vetory[e] = y
+
+   
+}
+for(var i = 0;i<vezes;i++){
+  var f =(3*vetorx[i])+4*(vetory[i])-5*(vetorz[i])
+  if(f===0){
+  msg+="x="+vetorx[i]+", y="+vetory[i]+", z="+vetorz[i]+"\n"
+  }
+}
+alert(msg)
