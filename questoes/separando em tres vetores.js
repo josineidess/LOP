c67db94a627f83d6d0bb6b425452660fda297f6a
JosineideSS <josineide1402@gmail.com>
2019-11-02 13:14:15 -0300

    Update separando em tres vetores.js

--- questoes/separando em tres vetores.js
+++ questoes/separando em tres vetores.js
@@ -1,3 +1,35 @@
+var vetor_geral = []
+var vetor_m_3 = []
+var vetor_m_4 = []
+var vetor_nm = []
+for(var e = 0;e < 20;e++){
+    var n = parseInt(prompt("numero: "))
+    vetor_geral[e] = n
+}
+for(var e = 0,m_3 = 0,m_4 = 0,nm = 0;e < 20;e++){
+    if(vetor_geral[e]%3 == 0 || vetor_geral[e]%4==0){
+        if(vetor_geral[e]%3 == 0 && vetor_geral[e]%4==0){
+            vetor_m_3[m_3]=vetor_geral[e]
+            vetor_m_4[m_4]=vetor_geral[e]
+            m_4++
+            m_4++
+        }else{
+            if(vetor_geral[e]%3==0){
+                vetor_m_3[m_3] = vetor_geral[e]
+                m_3++
+            }else{
+                vetor_m_4[m_4]=vetor_geral[e]
+                m_4++
+            }
+        }
+    }else{
+        vetor_nm[nm] = vetor_geral[e]
+        nm++
+    }
+}
+alert("a)"+vetor_m_3+"\nb)"+vetor_m_4+"\nc)"+vetor_nm)
+/*usando o push*/
+
 var vetora = []
 var vetorb = []
 var vetorc = []
