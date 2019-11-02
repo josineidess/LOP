60798c5625af08da73cc8cd631e52c68ce6afe71
JosineideSS <josineide1402@gmail.com>
2019-11-02 12:04:14 -0300

    Update Elementos do vetor múltiplos de 3 e de 4 simultaneamente.js

--- questoes/Elementos do vetor múltiplos de 3 e de 4 simultaneamente.js
+++ questoes/Elementos do vetor múltiplos de 3 e de 4 simultaneamente.js
@@ -1,4 +1,18 @@
 var vetor = []
+var n = parseInt(prompt("numero: "))
+for(var e = 0;e<n;e++){
+    var num = parseInt(prompt("num: "))
+    if(num % 3 == 0 && num % 4 == 0 && num != 0){
+        vetor[e] = num
+    }else{
+        e--
+    }
+}
+alert(vetor)
+
+/*com while*/
+
+var vetor = []
 var q = prompt('quant: ')
 //o while repete o número de vezes da entrada q
 while(q > 0){
@@ -8,7 +22,7 @@ while(q > 0){
         //esse é um decremento é a mesma coisa que q = q - 1 ou q-=1
         q--
         //se cumprir as condições ele é adicionado
-        vetor.push(num)
+        vetor[q] = num
     }
 }
 alert(vetor)
