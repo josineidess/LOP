1cb770f7c75e473d96c7e9c870efaa50586a3f68
JosineideSS <josineide1402@gmail.com>
2019-11-02 12:53:35 -0300

    Update Separando Vetor.js

--- questoes/Separando Vetor.js
+++ questoes/Separando Vetor.js
@@ -1,3 +1,23 @@
+var vetor_geral = []
+var vetor_par = []
+var vetor_impar = []
+for(var e = 0;e < 20;e++){
+    var n = parseInt(prompt("num: "))
+    vetor_geral[e] = n
+}
+for(var e = 0,p=0,i=0;e < 20;e++){
+    if(vetor_geral[e]%2==0){
+        vetor_par[p]=vetor_geral[e]
+        p++
+    } else{
+        vetor_impar[i]=vetor_geral[e]
+        i++
+    }
+}
+alert("Pares: "+vetor_par+"\nImpares: "+vetor_impar)
+
+/*usando o método push*/
+
 var vetor = []
 var vetor_par = []
 var vetor_impar = []
