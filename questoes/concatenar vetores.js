d92036d148ef80fed8601ebc1a57eb08c676eb8c
JosineideSS <josineide1402@gmail.com>
2019-11-02 12:47:37 -0300

    Update concatenar vetores.js

--- questoes/concatenar vetores.js
+++ questoes/concatenar vetores.js
@@ -1,3 +1,28 @@
+var ta = parseInt(prompt("tamanho a:"))
+var tb = parseInt(prompt("tamanho b: "))
+var vetora = []
+var vetorb = []
+var vetorc = []
+for(var e = 0;e<ta;e++){
+    vetora[e] = parseInt(prompt("num: "))
+}
+for(var e = 0;e<tb;e++){
+    vetorb[e] = parseInt(prompt("num: "))
+}
+for(var e = 0,r=ta,k=0;e<(ta+tb);e++){
+      if(r>0){
+          vetorc[e]=vetora[e]
+      }else{
+          vetorc[e]=vetorb[k]
+          k++
+      }
+      r--
+    }
+
+alert(vetorc)
+
+/* usando o método concat*/
+
 var qa = parseInt(prompt("quantidade vetor1: "))
 var qb = parseInt(prompt("quantidade vetor2: "))
 a = []
