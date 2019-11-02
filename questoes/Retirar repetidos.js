a49655905d147acabc9c2b00f302b08f3765fabf
JosineideSS <josineide1402@gmail.com>
2019-11-02 11:57:40 -0300

    Update Retirar repetidos.js

--- questoes/Retirar repetidos.js
+++ questoes/Retirar repetidos.js
@@ -1,4 +1,36 @@
-//pra que complicar a vida se temos ES6 agr?
+var n = parseInt(prompt("tamanho: "))
+var vetor = []
+var vetorb = []
+var msg = ''
+for(var e = 0;e<n;e++){
+    vetor[e] = parseInt(prompt("valor: "))
+}
+for(var i = 0; i < n;i++){
+        if(i>0){
+            for(var r = 0;r<i;r++){
+                if(vetorb[r]==vetor[i]){
+                    vetorb[i]=0
+                    
+                }
+            } 
+            if(vetorb[i]!=0){
+                vetorb[i]=vetor[i]
+                msg+=vetorb[i]+" "
+            }
+        
+        }else{
+            vetorb[i] = vetor[i]
+            msg+=vetorb[i]+" "
+        } 
+}
+alert(msg)
+
+ | 
+ |
+ |
+\ /
+ 
+//mas pra que complicar a vida se temos ES6 agr?
 var quant = prompt('quant: ')
 var vetor = []
 for(let e = 0;e < quant; e ++){
